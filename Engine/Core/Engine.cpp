401b517b1c4403a883f922c5b9536f77536d7cbf
rowleyi <155793517+rowleyi@users.noreply.github.com>
2026-04-06 17:07:30 -0700

    Establish Engine class with basic methods
    Added initial implementation of Engine class with core methods.

---
+++ Engine/Core/Engine.cpp
@@ -0,0 +1,24 @@
+#include "Engine.hpp"
+
+using namespace EchoXStudios;
+using namespace EchoCore;
+
+bool Engine::Initialize()
+{
+
+}
+
+void Engine::Update()
+{
+
+}
+
+void Engine::Draw()
+{
+
+}
+
+void Engine::Shutdown()
+{
+
+}
